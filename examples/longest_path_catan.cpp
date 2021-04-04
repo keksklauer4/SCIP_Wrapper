0a6a77e4817dfbc932ce58ccede814ca16c48387
Keksklauer4 <keksklauer4@gmail.com>
2021-04-04 00:42:49 -0700

    Fixed a minor bug and added another test for the longest path in catan board game.

--- examples/longest_path_catan.cpp
+++ examples/longest_path_catan.cpp
@@ -137,25 +137,67 @@ void milp_examples::longest_path_problem_catan(const std::vector<EdgePair> &edge
 
 namespace
 {
+  std::vector<fuint32_t> findLongestSubpath(std::multimap<fuint32_t, fuint32_t> remainingEdges, fuint32_t currentNode)
+  { // not a good style...
+    std::vector<fuint32_t> nodes{};
+    nodes.reserve(remainingEdges.size());
+    while(!remainingEdges.empty())
+    {
+      auto count = remainingEdges.count(currentNode);
+      if (count == 0) return nodes;
+      auto it = remainingEdges.find(currentNode);
+      if (count == 1)
+      {
+        currentNode = it->second;
+        nodes.push_back(currentNode);
+        remainingEdges.erase(it);
+      }
+      else if (count == 2)
+      { // can happen at most once (at the start node)
+        fuint32_t nextNodeA = it->second;
+        it++;
+        fuint32_t nextNodeB = it->second;
+        auto eraseIt = remainingEdges.equal_range(currentNode);
+        remainingEdges.erase(eraseIt.first, eraseIt.second);
+
+        // test taking path A
+        auto e = remainingEdges.insert(std::make_pair(currentNode, nextNodeB));
+        auto pathA = findLongestSubpath(remainingEdges, nextNodeA);
+        remainingEdges.erase(e);
+        remainingEdges.insert(std::make_pair(currentNode, nextNodeA));
+        auto pathB = findLongestSubpath(remainingEdges, nextNodeB);
+
+        if (pathA.size() > pathB.size())
+        {
+          nodes.push_back(nextNodeA);
+          nodes.insert(nodes.end(), pathA.begin(), pathA.end());
+        }
+        else
+        {
+          nodes.push_back(nextNodeB);
+          nodes.insert(nodes.end(), pathB.begin(), pathB.end());
+        }
+        break;
+      }
+      else
+      {
+        std::cerr << "Bug occurred..." << std::endl;
+        break;
+      }
+    }
+
+    return nodes;
+  }
 
   void printPath(const LongestPathResult& res)
   {
     std::multimap<fuint32_t, fuint32_t> edgesRemaining{res.edgesContained};
 
-    fuint32_t currentNode = res.startNode;
-    std::cout << std::endl << "Printing path:" << std::endl
-              << currentNode;
-    while (!edgesRemaining.empty())
+    auto nodes = findLongestSubpath(edgesRemaining, res.startNode);
+    std::cout << res.startNode;
+    for (auto node : nodes)
     {
-      auto it = edgesRemaining.find(currentNode);
-      if (it == edgesRemaining.end())
-      {
-        std::cerr << "BUG..." << std::endl;
-        return;
-      }
-      currentNode = it->second;
-      std::cout << "-->" << currentNode;
-      edgesRemaining.erase(it); // remove edge
+      std::cout << "-->" << node;
     }
     std::cout << std::endl;
   }
@@ -252,10 +294,77 @@ namespace
     milp_examples::longest_path_problem_catan(edges, nodes, res);
     printPath(res);
   }
+
+  void redditTest()
+  { // example from a question asked on reddit
+    // question can be found at: https://www.reddit.com/r/algorithms/comments/mh2n7k/how_do_i_find_the_longest_path_in_this_monstrosity/
+
+    /* The graph looks as follows:
+     *
+     *                    1    2
+     *                     \  /
+     *                      3
+     *                      |
+     *                      4
+     *                    /   \
+     *             19    21    5
+     *             |     |     |
+     *             18    20    6
+     *              \   /       \
+     *               17          7
+     *               |           |
+     *               16    10    8
+     *              /  \  /  \  /
+     *            15    11    9
+     *            |     |
+     *            14    12
+     *             \   /
+     *              13
+     *
+     * The optimal solution has length 18, starts at node 4 and ends at node 2. The longest route takes the following roads:
+     *    4-->21-->20-->17-->16-->15-->14-->13-->12-->11-->10-->9-->8-->7-->6-->5-->4-->3-->2
+     */
+
+    std::vector<fuint32_t> nodes
+    {
+      1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21
+    };
+
+    std::vector<EdgePair> edges
+    {
+      EdgePair(1,3),
+      EdgePair(2,3),
+      EdgePair(3,4),
+      EdgePair(4,5),
+      EdgePair(4,21),
+      EdgePair(5,6),
+      EdgePair(6,7),
+      EdgePair(7,8),
+      EdgePair(8,9),
+      EdgePair(9,10),
+      EdgePair(10,11),
+      EdgePair(11,12),
+      EdgePair(11,16),
+      EdgePair(12,13),
+      EdgePair(13,14),
+      EdgePair(14,15),
+      EdgePair(15,16),
+      EdgePair(16,17),
+      EdgePair(17,18),
+      EdgePair(17,20),
+      EdgePair(18,19),
+      EdgePair(20,21)
+    };
+    LongestPathResult res{};
+
+    milp_examples::longest_path_problem_catan(edges, nodes, res);
+    printPath(res);
+  }
 }
 
 int main()
 {
   //minimalTest();
-  bigTest();
+  //bigTest();
+  redditTest();
 }
